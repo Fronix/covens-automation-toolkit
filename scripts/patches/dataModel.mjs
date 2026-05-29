3378b224e8638158ee9fb02f4075126884fbda7d
Chris <chrisk123999@gmail.com>
2026-05-29 14:45:04 -0400

    Remove ConfigValue

--- scripts/patches/dataModel.mjs
+++ scripts/patches/dataModel.mjs
@@ -32,20 +32,19 @@ item.flags.cat.rollModifiers = [
                 requireAll: false
             },
             damageTypes: {
-                configValue: 'damageTypes',
+                value: ['fire', 'lightning']
                 requireAll: false
             }
         }
     }
 ]
 item.flags.cat.alternateFormula = {
-    value: '1d8 + @mod',
-    configValue: 'formula'
+    value: '1d8 + @mod'
 }
 */
-function checkReq(requirement, itemData, targetItem, defaultRequireAll = true) {
+function checkReq(requirement, itemData, defaultRequireAll = true) {
     if (!requirement) return true;
-    const reqValues = requirement.configValue ? (automationUtils.getConfig(targetItem, requirement.configValue) ?? []) : requirement.value;
+    const reqValues = requirement.value;
     const requireAll = requirement.requireAll ?? defaultRequireAll;
     if (itemData instanceof Set) {
         return requireAll ? reqValues.every(v => itemData.has(v))  : reqValues.some(v => itemData.has(v));
@@ -58,20 +57,20 @@ function checkReq(requirement, itemData, targetItem, defaultRequireAll = true) {
 function isValidModifier(modDef, targetItem, identifier, {rollData} = {}) {
     const reqs = modDef.restrictions;
     if (!reqs) return true;
-    if (!checkReq(reqs.identifier, identifier, targetItem, false)) return false;
-    if (!checkReq(reqs.type, targetItem.type, targetItem, false)) return false;
-    if (!checkReq(reqs.property, targetItem.system.properties, targetItem, true)) return false;
+    if (!checkReq(reqs.identifier, identifier, false)) return false;
+    if (!checkReq(reqs.type, targetItem.type, false)) return false;
+    if (!checkReq(reqs.property, targetItem.system.properties, true)) return false;
     if (targetItem.type === 'spell') {
-        if (!checkReq(reqs.school, targetItem.system.school, targetItem, false)) return false;
-        if (!checkReq(reqs.level, targetItem.system.level, targetItem, false)) return false;
-        if (!checkReq(reqs.method, targetItem.system.method, targetItem, false)) return false;
-        if (!checkReq(reqs.ability, targetItem.system.ability, targetItem, false)) return false;
+        if (!checkReq(reqs.school, targetItem.system.school, false)) return false;
+        if (!checkReq(reqs.level, targetItem.system.level, false)) return false;
+        if (!checkReq(reqs.method, targetItem.system.method, false)) return false;
+        if (!checkReq(reqs.ability, targetItem.system.ability, false)) return false;
     }
-    if (!checkReq(reqs.classIdentifier, targetItem.system.classIdentifier, targetItem, false)) return false;
+    if (!checkReq(reqs.classIdentifier, targetItem.system.classIdentifier, false)) return false;
     if (reqs.damageTypes) {
         if (!rollData) return false; 
         const currentTypes = rollData.options?.types || [rollData.options?.type];
-        if (!checkReq(reqs.damageTypes, currentTypes, targetItem, false)) return false;
+        if (!checkReq(reqs.damageTypes, currentTypes, false)) return false;
     }
     return true;
 }
@@ -104,7 +103,7 @@ function formula(wrapped) {
         if (item.type != 'feat') return;
         const altFormula = item.flags.cat?.alternateFormula?.[identifier];
         if (altFormula) {
-            const resolvedFormula = altFormula.configValue ? automationUtils.getConfig(targetItem, altFormula.configValue) : altFormula.value;
+            const resolvedFormula = altFormula.value;
             if (resolvedFormula) alternateFormulas.push(formula);
         }
         const modifiersList = item.flags.cat?.rollModifiers;
